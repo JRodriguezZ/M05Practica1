4e60973dfc26718b1c320b73aed716cd50772b30
JRodriguezz <jrodriguezz00@elpuig.xeill.net>
2021-02-02 18:10:12 +0100

    v1

--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -4,5 +4,9 @@ public class Main {
 
     public static void main(String[] args) {
         System.out.println("Hola mundo");
+
+        String b = "Hola";
+
+        System.out.println(b);
     }
 }
